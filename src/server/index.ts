53256b42f135aa2ef93dfe83dfce14d73167d348
Blaine Instone <blaine@instones.org>
2016-10-15 15:06:46 -0400

    boi

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 import IndexRoute from "../routes";
 import AnimalRoute from"../routes/AnimalRoutes/AnimalRoute";
-import AnimalCat from"../routes/AnimalRoutes/AnimalCat"
+import AnimalCat from"../routes/AnimalRoutes/sunshine"
 import AnimalDog from"../routes/AnimalRoutes/rain"
 import AnimalBird from"../routes/AnimalRoutes/extreme"
 
@@ -17,9 +17,9 @@ export default class Server {
                 const router = express.Router();
                 router.get("/", new IndexRoute().load);
                 router.get("/animal", new AnimalRoute().load);
-                router.get("/cat", new AnimalCat().load);
-                router.get("/rain", new AnimalDog().load);
-                router.get("/extreme", new AnimalBird().load);
+                router.get("/sunshine", new sunshine().load);
+                router.get("/rain", new rain().load);
+                router.get("/extreme", new extreme().load);
                 this.app.use(router);
         }
  
