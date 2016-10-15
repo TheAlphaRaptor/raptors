a0840e67164331eab22700798ff6148b2cba3b8d
Blaine Instone <blaine@instones.org>
2016-10-15 16:09:04 -0400

    l

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ var app = express();
 export default class IndexRoute {
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
-        res.send("<p> Here are the current directories: \n Meme \n Cat \n Dog </p>"); 
+        res.send("<p> Add these to the URl for better effects: \n extreme \n rain \n sunshine </p>"); 
 
     }
     
