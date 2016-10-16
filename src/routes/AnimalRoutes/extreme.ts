2738c9c731e0b4fd119105c0facaa92f4a3c212a
Blaine Instone <blaine@instones.org>
2016-10-16 10:13:02 -0400

    h

--- src/routes/AnimalRoutes/extreme.ts
+++ src/routes/AnimalRoutes/extreme.ts
@@ -3,10 +3,10 @@ import * as express from "express";
 export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
-        res.send("woah there")
+        res.send("<img src= http://wallpapercave.com/wp/8IF8Orn.jpg width=750><iframe width=560 height=315 src=https://www.youtube.com/embed/72aLUzHxnHM frameborder=0 allowfullscreen></iframe>");
  //      res.addListener("Nutshack.mp3"); 
 //var Audio: new (src: "..assests/musics/Nutshack.mp3")=> HTMLAudioElement
-        //res.send("Nutshack.mp3");
+ //   res.send("<audio controls autoplay><source src=  type=audio/mp3></audio>");
+    //res.send("<iframe width=560 height=315 src=https://www.youtube.com/embed/72aLUzHxnHM frameborder=0 allowfullscreen></iframe>");
     }
- //   AudioListener
 }
--- src/routes/AnimalRoutes/sunshine.ts
+++ src/routes/AnimalRoutes/sunshine.ts
@@ -2,13 +2,15 @@ import * as express from "express";
 
 export default class AnimalRoute {
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
-
-        res.sendFile(__dirname + "/assets/Images/sun.jpg")
+        res.type("text/html");
+        res.send("<img src= http://dykn.com/wp-content/uploads/2014/12/positively-smitten.jpg width=750>");
+  //      res.send("yeah boi"); "); 
+       // res.sendFile(__dirname + "/assets/Images/sun.jpg")
     }   
 }
 
-        res.type("text/html");
-        res.send("yeah boi"); 
-    }   
- }    
+    //    res.type("text/html");
+  //      res.send("yeah boi"); 
+  //  }   
+// }    
 
