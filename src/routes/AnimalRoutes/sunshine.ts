560016dc45bd6f1b16a429615584e72c4ad3fc70
Junbo Chen <thebo8800@gmail.com>
2016-10-16 03:03:37 -0400

    l

--- src/routes/AnimalRoutes/sunshine.ts
+++ src/routes/AnimalRoutes/sunshine.ts
@@ -4,6 +4,7 @@ export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
         res.send("yeah boi");
+        document.createElement("2596-sunshine-1920x1080-nature-wallpaper");
     }   
 }
     
