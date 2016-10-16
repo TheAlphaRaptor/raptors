8a94c878b2a23d09ac2804a05c7c62d003126e08
Blaine Instone <blaine@instones.org>
2016-10-16 10:18:09 -0400

    b

--- src/routes/AnimalRoutes/extreme.ts
+++ src/routes/AnimalRoutes/extreme.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
-        res.send("<img src= http://wallpapercave.com/wp/8IF8Orn.jpg width=750><iframe width=560 height=315 src=https://www.youtube.com/embed/72aLUzHxnHM frameborder=0 allowfullscreen></iframe>");
+        res.send("<img src= http://wallpapercave.com/wp/8IF8Orn.jpg width=750> <iframe width=560 height=315 src=https://www.youtube.com/embed/72aLUzHxnHM frameborder=0 allowfullscreen></iframe>");
  //      res.addListener("Nutshack.mp3"); 
 //var Audio: new (src: "..assests/musics/Nutshack.mp3")=> HTMLAudioElement
  //   res.send("<audio controls autoplay><source src=  type=audio/mp3></audio>");
