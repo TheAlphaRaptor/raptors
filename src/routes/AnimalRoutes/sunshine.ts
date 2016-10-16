d9a5a09d2dff0539b989982a837d44e0eae9c301
Junbo Chen <thebo8800@gmail.com>
2016-10-16 03:31:52 -0400

    l

--- src/routes/AnimalRoutes/sunshine.ts
+++ src/routes/AnimalRoutes/sunshine.ts
@@ -4,22 +4,5 @@ export default class AnimalRoute {
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
         res.send("yeah boi"); 
-        res.send("yeah boi");
     }   
  }    
-
-        document.createElement("img");
-
-interface Window {
-    Image: {   
-        prototype: HTMLImageElement;
-        new (): HTMLImageElement;
-    };
-}
-var a = new Images()
-
-    }   
-}
-
-    
->>>>>>> a960701ab57509b9367c5355af286b026795f7f0
