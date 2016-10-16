27cd634b78ac361ebbe946403b21ef365dd62d8e
Blaine Instone <blaine@instones.org>
2016-10-16 03:03:09 -0400

    g

--- src/routes/AnimalRoutes/extreme.ts
+++ src/routes/AnimalRoutes/extreme.ts
@@ -4,8 +4,9 @@ export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
         res.send("woah there")
-       // res.addListener(); 
-
+ //      res.addListener("Nutshack.mp3"); 
+//var Audio: new (src: "..assests/musics/Nutshack.mp3")=> HTMLAudioElement
+        //res.send("Nutshack.mp3");
     }
-    
+ //   AudioListener
 }
--- src/routes/AnimalRoutes/sunshine.ts
+++ src/routes/AnimalRoutes/sunshine.ts
@@ -2,18 +2,8 @@ import * as express from "express";
 
 export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
-//<<<<<<< HEAD
-        res.type("text/html");
-        res.send("yeah boi");
-  // res.send("C:\Users\Junbo\Desktop\raptors\src\routes\AnimalRoutes\assests\Images\2596-sunshine-1920x1080-nature-wallpaper.jpg");
-     res.send('<img src="/images/sun.jpg" id="sun" style="display:none"/>'"');
-     var img: HTMLImageElement;
-    var x: number = 50;
-    var y: number = 50;
-    
-    }   
-//=======
 
+        res.type("text/html");
+        res.send("yeah boi"); 
+    }   
     }
-//>>>>>>> a007d30d2c3f4e82bade920b253086c2b4255df7
-    
