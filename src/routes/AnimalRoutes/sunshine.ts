24d5bd52932f84df35a5e6dace41b23d1dfb3b85
Blaine Instone <blaine@instones.org>
2016-10-16 08:52:51 -0400

    c

--- src/routes/AnimalRoutes/sunshine.ts
+++ src/routes/AnimalRoutes/sunshine.ts
@@ -2,13 +2,13 @@ import * as express from "express";
 
 export default class AnimalRoute {
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
-<<<<<<< HEAD
+
         res.sendFile(__dirname + "/assets/Images/sun.jpg")
     }   
 }
-=======
+
         res.type("text/html");
         res.send("yeah boi"); 
     }   
  }    
->>>>>>> fab4a839db8f881192eb86202c912899850e9be5
+
