0f0f20bf4ddca5a0adf6e263de5cec9682271031
Blaine Instone <blaine@instones.org>
2016-10-16 02:28:44 -0400

    f

--- src/routes/AnimalRoutes/sunshine.ts
+++ src/routes/AnimalRoutes/sunshine.ts
@@ -6,7 +6,11 @@ export default class AnimalRoute{
         res.type("text/html");
         res.send("yeah boi");
   // res.send("C:\Users\Junbo\Desktop\raptors\src\routes\AnimalRoutes\assests\Images\2596-sunshine-1920x1080-nature-wallpaper.jpg");
-     
+     res.send('<img src="/images/sun.jpg" id="sun" style="display:none"/>'"');
+     var img: HTMLImageElement;
+    var x: number = 50;
+    var y: number = 50;
+    
     }   
 //=======
 
