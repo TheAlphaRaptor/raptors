7ad4e0565fd8e1a57ddd42cc77f8229e00cc3254
Junbo Chen <thebo8800@gmail.com>
2016-10-15 20:28:40 -0400

    l

--- src/routes/AnimalRoutes/AnimalRoute.ts
+++ src/routes/AnimalRoutes/AnimalRoute.ts
@@ -4,8 +4,9 @@ export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
         res.send("<p> Here are the current directories: \n extreme \n sunshine \n rain </p>"); 
-
-    }
+        res.type("text/html");
+        res.send("Turn ur speaker up :).")
+    
 
     
 }
