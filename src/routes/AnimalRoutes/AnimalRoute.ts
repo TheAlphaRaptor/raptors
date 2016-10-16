2da60c479d494bc1e9c6cdcc8f4ab3f80ee7a085
Junbo Chen <thebo8800@gmail.com>
2016-10-16 10:42:09 -0400

    l

--- src/routes/AnimalRoutes/AnimalRoute.ts
+++ src/routes/AnimalRoutes/AnimalRoute.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
-        res.send("<p> Here are the current  UR MOM directories: \n extreme \n sunshine \n rain </p> <img src = https://pbs.twimg.com/profile_images/1327289058/sac_logo.png>"); 
+        res.send("<p> Here are the current  UR MOM directories: \n extreme \n sunshine \n rain </p> <img src = http://hengplasticmx.eu/wp-content/uploads/2014/08/blue-and-green-plastic-bottles1.jpg>"); 
         
 }
 
