c8cf5aa20d8b8bb3fe61c11d9e52303f9961f833
Junbo Chen <thebo8800@gmail.com>
2016-10-16 10:38:13 -0400

    l

--- src/routes/AnimalRoutes/AnimalRoute.ts
+++ src/routes/AnimalRoutes/AnimalRoute.ts
@@ -3,8 +3,8 @@ import * as express from "express";
 export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
-        res.send("<p> Here are the current directories: \n extreme \n sunshine \n rain </p>"); 
-
+        res.send("<p> Here are the current  UR MOM directories: \n extreme \n sunshine \n rain </p> <img src = https://pbs.twimg.com/profile_images/1327289058/sac_logo.png>"); 
+        
 }
 
 }
