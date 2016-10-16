a040c379340195a0dbb32b6a4ca5e5c1f1531b20
Junbo Chen <thebo8800@gmail.com>
2016-10-16 02:40:38 -0400

    l

--- src/routes/AnimalRoutes/sunshine.ts
+++ src/routes/AnimalRoutes/sunshine.ts
@@ -4,7 +4,7 @@ export default class AnimalRoute{
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
         res.send("yeah boi");
-       
+        res.attachment("http://www.meghantelpner.com/wp-content/uploads/2013/07/2596-sunshine-1920x1080-nature-wallpaper.jpg");
     
 
     }   
