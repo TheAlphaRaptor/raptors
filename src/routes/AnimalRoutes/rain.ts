5fb6413fdf758d061334ebf8ee5f28f182b18537
Junbo Chen <thebo8800@gmail.com>
2016-10-16 00:16:41 -0400

    l

--- src/routes/AnimalRoutes/rain.ts
+++ src/routes/AnimalRoutes/rain.ts
@@ -1,10 +1,22 @@
 import * as express from "express";
 
-export default class AnimalRoute{
+export default class AnimalRoute {
     public load(req: express.Request, res: express.Response, nex: express.NextFunction) {
         res.type("text/html");
-        res.send("sad days"); 
+        res.send("sad days");
+
+        <!DOCTYPE html>
+            <html>
+            <body>
+
+            <p>If a browser cannot find an image, it will display the alternate text: </p>
+
+                < img src= "http://www.meghantelpner.com/wp-content/uploads/2013/07/2596-sunshine-1920x1080-nature-wallpaper.jpg" alt= "HTML5 Icon" style= "width:128px;height:128px;" >
+
+                    </body>
+                    < /html>
+
 
     }
-    
+
 }
